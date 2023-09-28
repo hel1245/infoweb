e55e4dd9d965644b1dfbe8034cdd64993dd60b02
hel1245 <146300558+hel1245@users.noreply.github.com>
2023-09-28 09:50:05 +0900

    Add files via upload

---
+++ infowebsite/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const btn2 = document.getElementById('btn2');
+    
+    btn2.addEventListener('click', function() {
+        window.location.href = 'https://www.apple.com/';
+    });
+});
+
+
